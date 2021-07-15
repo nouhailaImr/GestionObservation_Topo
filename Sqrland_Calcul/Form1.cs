46c52db4779d16128250874438efdbd6c174ed68
Nouhaila <elimrani.nouhaila@gmail.com>
2021-07-15 08:22:14 -0700

    load txt

--- Sqrland_Calcul/Form1.cs
+++ Sqrland_Calcul/Form1.cs
@@ -69,10 +69,6 @@ namespace Sqrland_Calcul
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
